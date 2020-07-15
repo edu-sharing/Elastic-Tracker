b618ebb59cc52f304a67cf94ead31276aad07a09
Daniel Rudolph <rudolph@metaventis.com>
2020-07-15 16:49:34 +0200

    fix: com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException: Unrecognized field "link"

--- src/main/java/org/edu_sharing/elasticsearch/edu_sharing/client/WidgetV2.java
+++ src/main/java/org/edu_sharing/elasticsearch/edu_sharing/client/WidgetV2.java
@@ -4,9 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class WidgetV2 {
 
 	public static class Condition{
