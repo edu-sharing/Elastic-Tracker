18b2afbcc6e29d96424aae6e8e534e27fdbc88f5
Daniel Rudolph <rudolph@edu-sharing.net>
2021-03-22 12:30:12 +0100

    fix: reduce validate session calls

--- src/main/java/org/edu_sharing/elasticsearch/edu_sharing/client/EduSharingAuthentication.java
+++ src/main/java/org/edu_sharing/elasticsearch/edu_sharing/client/EduSharingAuthentication.java
@@ -8,12 +8,14 @@ import org.edu_sharing.elasticsearch.tracker.TransactionTracker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.util.Date;
 
 @Aspect
 @Component
@@ -24,10 +26,19 @@ public class EduSharingAuthentication {
     @Autowired
     private EduSharingClient eduSharingClient;
 
+    long lastTimeAuthChecked = 0;
+
+    @Value("${edusharing.sessionvalidate.delay}")
+    long eduSharingSessionValidateDelay;
+
     @Before("@annotation(org.edu_sharing.elasticsearch.edu_sharing.client.EduSharingAuthentication.ManageAuthentication)")
     public void manageAuthentication(JoinPoint joinPoint) throws Throwable {
-        logger.info("manageAuthentication for:" + joinPoint.getSignature().getName());
-        eduSharingClient.manageAuthentication();
+        Date d = new Date();
+        if(lastTimeAuthChecked == 0 || (d.getTime() - lastTimeAuthChecked) > eduSharingSessionValidateDelay) {
+            logger.info("manageAuthentication for:" + joinPoint.getSignature().getName());
+            eduSharingClient.manageAuthentication();
+            lastTimeAuthChecked = d.getTime();
+        }
     }
 
 
